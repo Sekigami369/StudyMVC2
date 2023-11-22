ae0263b47216c1317f843b658983d6f409b4af1b
sekigmai369 <skgmyum@gmail.com>
2023-11-22 22:45:33 +0900

    MVC firstcommit

---
+++ Data/StudyMVC2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudyMVC2.Models;
+
+namespace StudyMVC2.Data
+{
+    public class StudyMVC2Context : DbContext
+    {
+        public StudyMVC2Context(DbContextOptions<StudyMVC2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; } = default!;
+    }
+}
